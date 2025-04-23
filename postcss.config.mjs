8a014ef392faebdc7a818569b1768872ee89e7e3
husniabad2 <1ms19cs052@gmail.com>
2025-04-23 15:11:57 +0200

    Update postcss.config.mjs (add next standalone)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
+/** @type {import('next').NextConfig} */ // Recommended for better type checking
 const config = {
+  output: "standalone", // <--- Add this line to enable standalone output
   plugins: ["@tailwindcss/postcss"],
+
+  // You can add other Next.js configurations here if needed
+  // e.g., images: { unoptimized: true },
+  //       trailingSlash: true,
 };
 
 export default config;
